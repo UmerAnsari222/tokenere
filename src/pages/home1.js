b5c12c31e03a8f210c44dd18b0e934c4d20eabfd
umar <umerfaqrooq78@gmail.com>
2023-05-04 10:07:28 +0500

    Ading Some More Thing

--- src/pages/home1.js
+++ src/pages/home1.js
@@ -1,10 +1,12 @@
 import BottomBar from "../components/BottomBar";
 import CenterContent from "../components/CenterContent";
+import TopBar from "../components/TopBar";
 import "./home1.css";
 
 const Home1 = () => {
   return (
     <div>
+      <TopBar />
       <CenterContent />
       <BottomBar />
     </div>
